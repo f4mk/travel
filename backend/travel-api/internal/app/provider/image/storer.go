04bc81cc62fb6e7f872d1199e06ddeb6c8de61d4
Amir Astezhev <famkapav@gmail.com>
2023-09-23 00:35:00 +0200

    added imaginary and refacored grafana k8s configs

--- backend/travel-api/internal/app/provider/image/storer.go
+++ backend/travel-api/internal/app/provider/image/storer.go
@@ -1,1 +1,15 @@
 package image
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/rs/zerolog"
+)
+
+type Storer struct {
+	repo *sqlx.DB
+	log  *zerolog.Logger
+}
+
+func NewStorer(l *zerolog.Logger, r *sqlx.DB) *Storer {
+	return &Storer{repo: r, log: l}
+}
